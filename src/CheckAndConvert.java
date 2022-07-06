014adac047ff5134ab671021150a23f73bd129f4
Konstantin <94786218+Beges100@users.noreply.github.com>
2022-07-06 16:03:08 +0300

    Add files via upload
    - Make Exception with input
    - Exception (After input 0 with Romanian numbers)
    - Realise method "public static String calc(String input)"

--- src/CheckAndConvert.java
+++ src/CheckAndConvert.java
@@ -35,5 +35,18 @@ public class CheckAndConvert {
         return (x > 0 && x <= 10) && (y > 0 && y <= 10);
     }
 
-
+    static String convertResult(int result) {
+        String[] romanian = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+                "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+                "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+                "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+                "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+                "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
+        };
+        if (result > 0)
+            return romanian[result];
+        else
+            return"Данные введены неверно";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,35 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] line = sc.nextLine().split(" ");
-        if (CheckAndConvert.checkArab(line[0]) && CheckAndConvert.checkArab(line[2])) {
-            if (CheckAndConvert.checkCorrectFormat(Integer.parseInt(line[0]), Integer.parseInt(line[2]))) {
-                Calculation result = new Calculation(Integer.parseInt(line[0]),line[1],Integer.parseInt(line[2]));
-                System.out.println(result.calc());
-            } else {
-                System.out.println("Ввели неверное значение");
-            }
-        } else if (CheckAndConvert.checkRoma(line[0]) && CheckAndConvert.checkRoma(line[2])){
-             if (CheckAndConvert.checkCorrectFormat(CheckAndConvert.convertRoma(line[0]), CheckAndConvert.convertRoma(line[2]))) {
-                Calculation roma = new Calculation(CheckAndConvert.convertRoma(line[0]),line[1],CheckAndConvert.convertRoma(line[2]));
-                convertResult(roma.calc());
+        String input = sc.nextLine();
+        System.out.println(calc(input));
+    }
+
+    public static String calc(String input) {
+        String[] line = input.split(" ");
+        String returnedResult = "";
+        if (line.length <= 3) {
+            if (CheckAndConvert.checkArab(line[0]) && CheckAndConvert.checkArab(line[2])) {
+                if (CheckAndConvert.checkCorrectFormat(Integer.parseInt(line[0]), Integer.parseInt(line[2]))) {
+                    Calculation result = new Calculation(Integer.parseInt(line[0]),line[1],Integer.parseInt(line[2]));
+                    returnedResult = String.valueOf(result.calc());
+                } else {
+                    System.out.println("Ввели неверное значение");
+                }
+            } else if (CheckAndConvert.checkRoma(line[0]) && CheckAndConvert.checkRoma(line[2])){
+                if (CheckAndConvert.checkCorrectFormat(CheckAndConvert.convertRoma(line[0]), CheckAndConvert.convertRoma(line[2]))) {
+                    Calculation roma = new Calculation(CheckAndConvert.convertRoma(line[0]),line[1],CheckAndConvert.convertRoma(line[2]));
+                    returnedResult = CheckAndConvert.convertResult(roma.calc());
+                } else {
+                    System.out.println("Ввели неверное значение");
+                }
             } else {
                 System.out.println("Ввели неверное значение");
             }
         } else {
-            System.out.println("Ввели неверное значение");
+            returnedResult = "Введены некорректные данные";
         }
+        return returnedResult;
     }
 
-    static void convertResult(int result) {
-        String[] romanian = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
-                "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
-                "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
-                "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
-                "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
-                "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
-                "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
-        };
-        System.out.println(romanian[result]);
-    }
 }
